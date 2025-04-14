f7bdaa261050c7c31de66015f1a455346d55f4e5
Shibo Zheng <burning_7l@hotmail.com>
2025-04-13 20:55:03 -0700

    add function signature for cache read and cache write

--- Server/ServletServerAndConsumer/target/classes/sql/getResortSkiersDay.sql
+++
@@ -1,5 +0,0 @@
-SELECT COUNT(DISTINCT skier_id)
-FROM LiftRides
-WHERE resort_id = ?
-  AND season_id = ?
-  AND day_id = ?;
--- Server/ServletServerAndConsumer/target/classes/sql/getSkierDayVertical.sql
+++
@@ -1,6 +0,0 @@
-SELECT SUM(10 * lift_id)
-FROM LiftRides
-WHERE skier_id = ?
-  AND resort_id = ?
-  AND season_id = ?
-  AND day_id = ?;
