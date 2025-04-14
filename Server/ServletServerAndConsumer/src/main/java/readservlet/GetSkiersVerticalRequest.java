4dceaec0143319b07df0fecc2041e9487d7fd64b
yanghansin <yanghansin@outlook.com>
2025-04-14 13:31:29 -0700

    ReadServlet Impl complete.

---
+++ Server/ServletServerAndConsumer/src/main/java/readservlet/GetSkiersVerticalRequest.java
@@ -0,0 +1,20 @@
+package readservlet;
+
+public class GetSkiersVerticalRequest {
+    /**
+     * GET/skiers/{skierID}/vertical
+     * get the total vertical for the skier for specified seasons at the specified resort
+     */
+    public int skierID;
+    public String resortID;
+    public String seasonID;
+
+    public GetSkiersVerticalRequest(int skierID, String resortID, String seasonID) {
+
+        this.skierID = skierID;
+        this.resortID = resortID;
+        this.seasonID = seasonID;
+    }
+
+
+}
