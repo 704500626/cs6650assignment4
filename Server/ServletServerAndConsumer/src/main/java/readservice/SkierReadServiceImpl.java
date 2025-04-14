d9fd74c13942b1dbbc35c6e2ebb4ca8652f923b9
zoe-lry <151310537+zoe-lry@users.noreply.github.com>
2025-04-14 11:50:42 -0700

    update SkierReadServiceImpl.java align the methods name with CacheService (#17)

--- Server/ServletServerAndConsumer/src/main/java/readservice/SkierReadServiceImpl.java
+++ Server/ServletServerAndConsumer/src/main/java/readservice/SkierReadServiceImpl.java
@@ -1,13 +1,10 @@
 package readservice;
 
-import static java.util.Objects.*;
-
 import cacheservice.CacheReadService;
 import cacheservice.CacheWriteService;
 import dao.LiftRideReader;
 import io.grpc.stub.StreamObserver;
 import java.sql.SQLException;
-import java.util.Objects;
 import model.Configuration;
 import skierread.SkierReadServiceGrpc;
 import skierread.SkierReadServiceOuterClass.ResortDayRequest;
@@ -15,7 +12,6 @@ import skierread.SkierReadServiceOuterClass.SkierCountResponse;
 import skierread.SkierReadServiceOuterClass.SkierDayRequest;
 import skierread.SkierReadServiceOuterClass.VerticalIntResponse;
 import skierread.SkierReadServiceOuterClass.VerticalListResponse;
-import skierread.SkierReadServiceOuterClass.VerticalRecord;
 import skierread.SkierReadServiceOuterClass.VerticalRequest;
 import utils.ConfigUtils;
 
@@ -23,14 +19,14 @@ public class SkierReadServiceImpl extends SkierReadServiceGrpc.SkierReadServiceI
 
   private static final Configuration config = ConfigUtils.getConfigurationForLiftRideService();
 
-  private LiftRideReader liftRideReader = new LiftRideReader(config);
+  private final LiftRideReader liftRideReader = new LiftRideReader(config);
 
   @Override
   public void getTotalVertical(VerticalRequest request,
       StreamObserver<VerticalListResponse> responseObserver) {
     VerticalListResponse response = null;
     // Check if in the Redis Cache
-    response = CacheReadService.getTotalVertical(request.getResortID(), request.getSeasonID(),
+    response = CacheReadService.getTotalVerticalFromCache(request.getResortID(), request.getSeasonID(),
         request.getSkierID());
     if (response == null) { // if Not in Redis, get from the DB and then write to Redis
       try {
@@ -45,9 +41,8 @@ public class SkierReadServiceImpl extends SkierReadServiceGrpc.SkierReadServiceI
         throw new RuntimeException(e);
       }
       // write to Redis
-      CacheWriteService.writeVerticalList(request.getSkierID(),
+      CacheWriteService.writeVerticalListToCache(request.getSkierID(),
           request.getResortID(),
-          request.getSeasonID(),
           response.getRecordsList());
     }
     responseObserver.onNext(response);
@@ -59,14 +54,14 @@ public class SkierReadServiceImpl extends SkierReadServiceGrpc.SkierReadServiceI
       StreamObserver<VerticalIntResponse> responseObserver) {
     VerticalIntResponse response = null;
     // Check if in the Redis Cache
-    response = CacheReadService.getUniqueSkierCount(request.getResortID(), request.getSeasonID(),
+    response = CacheReadService.getTotalVerticalOfSkierFromCache(request.getResortID(), request.getSeasonID(),
         request.getDayID(), request.getSkierID());
     if (response == null) { // if Not in Redis, get from the DB and then write to Redis
       try {
         response = liftRideReader.getSkierDayVertical(request.getResortID(), request.getSeasonID(),
             request.getDayID(), request.getSkierID());
         // write to Redis
-        CacheWriteService.writeVertical(request.getResortID(),
+        CacheWriteService.writeVerticalToCache(request.getResortID(),
             request.getSeasonID(),
             request.getDayID(),
             request.getSkierID(),
@@ -93,7 +88,7 @@ public class SkierReadServiceImpl extends SkierReadServiceGrpc.SkierReadServiceI
             request.getSeasonID(),
             request.getDayID());
         // write to Redis
-        CacheWriteService.writeUniqueSkierCount(
+        CacheWriteService.writeUniqueSkierCountToCache(
             request.getResortID(),
             request.getSeasonID(),
             request.getDayID(),
