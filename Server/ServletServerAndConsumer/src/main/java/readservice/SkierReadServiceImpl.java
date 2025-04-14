4cf5ff5d25ce29177271653d2850a949f66f2eb1
zoe-lry <zoeliruiyi@gmail.com>
2025-04-13 21:45:10 -0700

    update SkierReadServiceImpl.java connect to Redis

--- Server/ServletServerAndConsumer/src/main/java/readservice/SkierReadServiceImpl.java
+++ Server/ServletServerAndConsumer/src/main/java/readservice/SkierReadServiceImpl.java
@@ -1,10 +1,13 @@
 package readservice;
 
+import static java.util.Objects.*;
+
 import cacheservice.CacheReadService;
 import cacheservice.CacheWriteService;
 import dao.LiftRideReader;
 import io.grpc.stub.StreamObserver;
 import java.sql.SQLException;
+import java.util.Objects;
 import model.Configuration;
 import skierread.SkierReadServiceGrpc;
 import skierread.SkierReadServiceOuterClass.ResortDayRequest;
@@ -17,54 +20,89 @@ import skierread.SkierReadServiceOuterClass.VerticalRequest;
 import utils.ConfigUtils;
 
 public class SkierReadServiceImpl extends SkierReadServiceGrpc.SkierReadServiceImplBase {
+
   private static final Configuration config = ConfigUtils.getConfigurationForLiftRideService();
-//  private static final CacheReadService cacheReadService = new CacheReadService();
-//  private static final CacheWriteService cacheWriteService = new CacheWriteService();
 
   private LiftRideReader liftRideReader = new LiftRideReader(config);
 
   @Override
-  public void getTotalVertical(VerticalRequest request, StreamObserver<VerticalListResponse> responseObserver) {
+  public void getTotalVertical(VerticalRequest request,
+      StreamObserver<VerticalListResponse> responseObserver) {
     VerticalListResponse response = null;
-    if (request.getSeasonID().isEmpty()) {
-      try {
-        response = liftRideReader.getSkierResortTotals(request.getSkierID(), request.getResortID());
-      } catch (SQLException e) {
-        throw new RuntimeException(e);
+    // Check if in the Redis Cache
+    response = CacheReadService.getTotalVertical(request.getResortID(), request.getSeasonID(),
+        request.getSkierID());
+    if (response == null) { // if Not in Redis, get from the DB and then write to Redis
+      if (request.getSeasonID().isEmpty()) {
+        try {
+          response = liftRideReader.getSkierResortTotals(request.getSkierID(),
+              request.getResortID());
+        } catch (SQLException e) {
+          throw new RuntimeException(e);
+        }
+      } else {
+        try {
+          response = liftRideReader.getSkierResortTotals(request.getSkierID(),
+              request.getResortID(), request.getSeasonID());
+        } catch (SQLException e) {
+          throw new RuntimeException(e);
+        }
       }
-    } else{
-      try {
-        response = liftRideReader.getSkierResortTotals(request.getSkierID(), request.getResortID(), request.getSeasonID());
-      } catch (SQLException e) {
-        throw new RuntimeException(e);
-      }
+      CacheWriteService.writeVerticalList(request.getSkierID(),
+                                          request.getResortID(),
+                                          request.getSeasonID(),
+                                          response.getRecordsList());
     }
     responseObserver.onNext(response);
     responseObserver.onCompleted();
   }
 
   @Override
-  public void getSkierDayRides(SkierDayRequest request, StreamObserver<VerticalIntResponse> responseObserver) {
+  public void getSkierDayRides(SkierDayRequest request,
+      StreamObserver<VerticalIntResponse> responseObserver) {
     VerticalIntResponse response = null;
-//     response = cacheReadService.getUniqueSkierCount(request.getResortID(), request.getSeasonID(),
-//         request.getDayID(), request.getSkierID());
-    try {
-      response = liftRideReader.getSkierDayVertical(request.getResortID(), request.getSeasonID(),
-          request.getDayID(), request.getSkierID());
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
+    // Check if in the Redis Cache
+    response = CacheReadService.getUniqueSkierCount(request.getResortID(), request.getSeasonID(),
+        request.getDayID(), request.getSkierID());
+    if (response == null) { // if Not in Redis, get from the DB and then write to Redis
+      try {
+        response = liftRideReader.getSkierDayVertical(request.getResortID(), request.getSeasonID(),
+            request.getDayID(), request.getSkierID());
+        // write to Redis
+        CacheWriteService.writeVertical(request.getResortID(),
+                                        request.getSeasonID(),
+                                        request.getDayID(),
+                                        request.getSkierID(),
+                                        response.getTotalVertical());
+      } catch (SQLException e) {
+        throw new RuntimeException(e);
+      }
     }
     responseObserver.onNext(response);
     responseObserver.onCompleted();
   }
 
   @Override
-  public void getResortDaySkiers(ResortDayRequest request, StreamObserver<SkierCountResponse> responseObserver) {
+  public void getResortDaySkiers(ResortDayRequest request,
+      StreamObserver<SkierCountResponse> responseObserver) {
     SkierCountResponse response = null;
-    try {
-      response = liftRideReader.getResortUniqueSkiers(request.getResortID(), request.getSeasonID(), request.getDayID());
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
+    // Check if in the Redis Cache
+    response = CacheReadService.getUniqueSkierCountFromCache(request.getResortID(), request.getSeasonID(),
+        request.getDayID());
+    if (response == null) { // if Not in Redis, get from the DB and then write to Redis
+      try {
+        response = liftRideReader.getResortUniqueSkiers(request.getResortID(),
+            request.getSeasonID(),
+            request.getDayID());
+        // write to Redis
+        CacheWriteService.writeUniqueSkierCount(
+                                        request.getResortID(),
+                                        request.getSeasonID(),
+                                        request.getDayID(),
+                                        response.getSkierCount());
+      } catch (SQLException e) {
+        throw new RuntimeException(e);
+      }
     }
     responseObserver.onNext(response);
     responseObserver.onCompleted();
